ce74849d2332cf1a1da33c4c602f576ff115dcb3
Ankit Kumar Suthar <93942764+iSuthar@users.noreply.github.com>
2025-11-06 17:03:19 +0530

    Add multi_sta_lta to core module exports

--- seiscae/core/__init__.py
+++ seiscae/core/__init__.py
@@ -1,11 +1,12 @@
-"""Core functionality for seisCAE."""
+"""Core detection and preprocessing modules."""
 
-from .detection import EventDetector
+from .detection import EventDetector, multi_sta_lta
 from .preprocessing import SpectrogramGenerator, EventExtractor
 from .catalog import EventCatalog
 
 __all__ = [
     'EventDetector',
+    'multi_sta_lta',
     'SpectrogramGenerator',
     'EventExtractor',
     'EventCatalog',
